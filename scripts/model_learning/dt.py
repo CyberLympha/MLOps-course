b37ac7bae0e569f320204e7c901eaa6790fb76e0
yurichernyshov <yuchernyshov@ussc.ru>
2022-10-11 16:25:36 +0500

    Decision tree learning stage was added

--- scripts/model_learning/dt.py
+++ scripts/model_learning/dt.py
@@ -1,20 +1,31 @@
 import sys
 import os
-import io
+import yaml
+import pickle
+
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
-if len(sys.argv) != 2:
+if len(sys.argv) != 3:
     sys.stderr.write("Arguments error. Usage:\n")
-    sys.stderr.write("\tpython get_features.py data-file\n")
+    sys.stderr.write("\tpython dt.py data-file model \n")
     sys.exit(1)
 
 f_input = sys.argv[1]
+f_output = os.path.join("models", sys.argv[2])
+os.makedirs(os.path.join("models"), exist_ok=True)
+
+params = yaml.safe_load(open("params.yaml"))["train"]
+p_seed = params["seed"]
+p_max_depth = params["max_depth"]
 
 df = pd.read_csv(f_input)
-
 X = df.iloc[:,[1,2,3]]
 y = df.iloc[:,0]
 
-model = DecisionTreeClassifier()
-model.fit(X, y)
+clf = DecisionTreeClassifier(max_depth=p_max_depth, random_state=p_seed)
+clf.fit(X, y)
+
+with open(f_output, "wb") as fd:
+    pickle.dump(clf, fd)
+
