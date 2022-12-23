170fb147a5650acbdc4dcf63eed76fe581c2c527
yurichernyshov <yuchernyshov@ussc.ru>
2022-12-23 10:59:29 +0500

    test scripts changed

--- scripts/tests/test1.py
+++ scripts/tests/test1.py
@@ -6,8 +6,8 @@ import json
 import pandas as pd
 
 df = pd.read_csv("/home/stage-srv/Titanic/datasets/stage4/test.csv")
-X = df.iloc[:,[0,1,2]]
-y = df.iloc[:,3]
+X = df.iloc[:,[1,2,3]]
+y = df.iloc[:,0]
 
 def test1():
     with open("/home/stage-srv/Titanic/models/model.pkl", "rb") as fd:
