1847a6c8a255ecf33a00935343dc854a84d69d39
yurichernyshov <yuchernyshov@ussc.ru>
2022-12-23 10:47:33 +0500

    test1.py path was corrected

--- scripts/tests/test1.py
+++ scripts/tests/test1.py
@@ -5,12 +5,12 @@ import json
 
 import pandas as pd
 
-df = pd.read_csv("./datasets/stage4/test.csv")
+df = pd.read_csv("../datasets/stage4/test.csv")
 X = df.iloc[:,[0,1,2]]
 y = df.iloc[:,3]
 
 def check1():
-    with open("./models/model.pkl", "rb") as fd:
+    with open("../models/model.pkl", "rb") as fd:
         clf = pickle.load(fd)
     score = clf.score(X, y)
     assert score > 0.75
