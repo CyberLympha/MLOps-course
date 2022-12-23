11028711e0e136312b81b38fc8e98521e8cdfc87
yurichernyshov <yuchernyshov@ussc.ru>
2022-12-23 10:53:31 +0500

    test name was changed from check to test

--- scripts/tests/test1.py
+++ scripts/tests/test1.py
@@ -9,7 +9,7 @@ df = pd.read_csv("/home/stage-srv/Titanic/datasets/stage4/test.csv")
 X = df.iloc[:,[0,1,2]]
 y = df.iloc[:,3]
 
-def check1():
+def test1():
     with open("/home/stage-srv/Titanic/models/model.pkl", "rb") as fd:
         clf = pickle.load(fd)
     score = clf.score(X, y)
