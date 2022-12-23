cea5c75deb400c153684ba4036f977d0b59f8c4e
yurichernyshov <yuchernyshov@ussc.ru>
2022-12-23 10:31:07 +0500

    test script was added

--- scripts/tests/test1.py
+++ scripts/tests/test1.py
@@ -5,14 +5,13 @@ import json
 
 import pandas as pd
 
-
-#df = pd.read_csv("./datasets/stage4")
-#X = df.iloc[:,[1,2,3]]
-#y = df.iloc[:,0]
+df = pd.read_csv("./datasets/stage4/test.csv")
+X = df.iloc[:,[0,1,2]]
+y = df.iloc[:,3]
 
 def check1():
     with open("./models/model.pkl", "rb") as fd:
         clf = pickle.load(fd)
-    score = clf.score()
-    assert score > 0.8
+    score = clf.score(X, y)
+    assert score > 0.75
 
