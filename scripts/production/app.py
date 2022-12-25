8f2ea875bb3ac8838f4c52f8bc40750f6ca3b89c
yurichernyshov <yuchernyshov@ussc.ru>
2022-12-25 14:46:30 +0500

    full path to model.pkl for production was added

--- scripts/production/app.py
+++ scripts/production/app.py
@@ -9,7 +9,7 @@ def hello():
 
 @app.route("/predict/<int:pclass>/<int:sex>/<int:age>")
 def predict(pclass, sex, age):
-    with open("./models/model.pkl", "rb") as fd:
+    with open("/home/prod-srv-admin/Titanic/models/model.pkl", "rb") as fd:
         clf = pickle.load(fd)
     prediction = int(clf.predict([[pclass, sex, age]])[0])
     return(jsonify({"survived": prediction}))
