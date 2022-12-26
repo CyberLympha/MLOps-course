97e4523e70d124133452ee427a38ab4fc0f8be17
yurichernyshov <yuchernyshov@ussc.ru>
2022-12-26 14:05:19 +0500

    error with order of features in stage4 was solved

--- scripts/data_processing/train_test_split.py
+++ scripts/data_processing/train_test_split.py
@@ -25,5 +25,5 @@ X = df.iloc[:,[1,2,3]]
 y = df.iloc[:,0]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=p_split_ratio, stratify=y)
 
-pd.concat([X_train, y_train], axis=1).to_csv(f_output_train, header=None, index=None)
-pd.concat([X_test, y_test], axis=1).to_csv(f_output_test, header=None, index=None)
+pd.concat([y_train, X_train], axis=1).to_csv(f_output_train, header=None, index=None)
+pd.concat([y_test, X_test], axis=1).to_csv(f_output_test, header=None, index=None)
